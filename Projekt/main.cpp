62c7e54fa8bd942965f603c478b8a507808cdda7
Boas Duenkel <boa.duenkel.16@lehre.mosbach.dhbw.de>
2017-09-20 10:46:12 +0200

    Logik des Maximums erstellt, 1. Mal ausgeführt

--- Projekt/main.cpp
+++ Projekt/main.cpp
@@ -4,7 +4,17 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    int feld[6] = {1,3,6,2,3,4};
+    int Max = 0;
+
+    for ( int i = 0 ; i < 6 ; i += 1 )
+    {
+        if ( feld[i] > Max)
+            Max = feld[i];
+    }
+
+    cout << Max << endl;
+
     return 0;
 }
 
